9dc74b43b561f35992ef9bd3e4b87fad1964a1eb
circler3 <circler3@sina.com>
2016-04-11 17:46:10 +0800

    add Client project

---
+++ PMClient/ViewModel/MainViewModel.cs
@@ -0,0 +1,93 @@
+﻿using GalaSoft.MvvmLight;
+using PMClient.Model;
+using System.Collections.ObjectModel;
+
+namespace PMClient.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// See http://www.mvvmlight.net
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private readonly IDataService _dataService;
+
+        /// <summary>
+        /// The <see cref="WelcomeTitle" /> property's name.
+        /// </summary>
+        public const string WelcomeTitlePropertyName = "WelcomeTitle";
+
+        private string _welcomeTitle = string.Empty;
+
+        /// <summary>
+        /// Gets the WelcomeTitle property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public string WelcomeTitle
+        {
+            get
+            {
+                return _welcomeTitle;
+            }
+            set
+            {
+                Set("WelcomeTitle", ref _welcomeTitle, value);
+            }
+        }
+
+        /// <summary>
+        /// The <see cref="WorkItems" /> property's name.
+        /// </summary>
+        public const string WorkItemsPropertyName = "WorkItems";
+
+        private ObservableCollection<WorkItemViewModel> _workItems = new ObservableCollection<WorkItemViewModel>();
+
+        /// <summary>
+        /// Gets the WelcomeTitle property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public ObservableCollection<WorkItemViewModel> WorkItems
+        {
+            get
+            {
+                return _workItems;
+            }
+            set
+            {
+                Set("WorkItems", ref _workItems, value);
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel(IDataService dataService)
+        {
+            _dataService = dataService;
+            _dataService.GetData(
+                (item, error) =>
+                {
+                    if (error != null)
+                    {
+                        // Report error here
+                        return;
+                    }
+
+                    WelcomeTitle = item.Title;
+                });
+            _workItems.Add(new WorkItemViewModel() { Name = "A work item", Percentage = 30 });
+            _workItems.Add(new WorkItemViewModel() { Name = "B work item", Percentage = 40 });
+        }
+
+      
+
+        ////public override void Cleanup()
+        ////{
+        ////    // Clean up if needed
+
+        ////    base.Cleanup();
+        ////}
+    }
+}
---
+++ PMClient/ViewModel/WorkItemViewModel.cs
@@ -0,0 +1,116 @@
+﻿using GalaSoft.MvvmLight;
+using System;
+
+namespace PMClient.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that a View can data bind to.
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class WorkItemViewModel : ViewModelBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the WorkItemsViewModel class.
+        /// </summary>
+        public WorkItemViewModel()
+        {
+
+        }
+
+        /// <summary>
+        /// The <see cref="Name" /> property's name.
+        /// </summary>
+        public const string NamePropertyName = "Name";
+
+        private string _name = string.Empty;
+
+        /// <summary>
+        /// Gets the WelcomeTitle property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                Set("Name", ref _name, value);
+            }
+        }
+
+        /// <summary>
+        /// The <see cref="Deadline" /> property's name.
+        /// </summary>
+        public const string DeadlinePropertyName = "Deadline";
+
+        private DateTime _deadline = DateTime.Now;
+
+        /// <summary>
+        /// Gets the WelcomeTitle property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public DateTime Deadline
+        {
+            get
+            {
+                return _deadline;
+            }
+            set
+            {
+                Set("Deadline", ref _deadline, value);
+            }
+        }
+
+        /// <summary>
+        /// The <see cref="Name" /> property's name.
+        /// </summary>
+        public const string PercentagePropertyName = "Percentage";
+
+        private int _percentage = 0;
+
+        /// <summary>
+        /// Gets the WelcomeTitle property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public int Percentage
+        {
+            get
+            {
+                return _percentage;
+            }
+            set
+            {
+                Set("Percentage", ref _percentage, value);
+            }
+        }
+
+        /// <summary>
+        /// The <see cref="Description" /> property's name.
+        /// </summary>
+        public const string DescriptionPropertyName = "Description";
+
+        private string _description = string.Empty;
+
+        /// <summary>
+        /// Gets the WelcomeTitle property.
+        /// Changes to that property's value raise the PropertyChanged event. 
+        /// </summary>
+        public string Description
+        {
+            get
+            {
+                return _description;
+            }
+            set
+            {
+                Set("Description", ref _description, value);
+            }
+        }
+
+        public int Priority { get; set; }
+    }
+}
